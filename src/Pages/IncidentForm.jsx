5ea4d6103c096212604a3da5a21f4a3ecba3589e
Okobi Eigbe <56739652+okobi45@users.noreply.github.com>
2026-04-15 20:33:53 +0100

    Fixed Typos and code syntax

--- src/Pages/IncidentForm.jsx
+++ src/Pages/IncidentForm.jsx
@@ -11,7 +11,7 @@ function IncidentForm() {
                     <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
                         Secure Web Reporting (SWR) is a community based platform to ensure only registered reporters
                         can report incidents in the community. For more information on this see the TOS. Please NOTE that
-                        all reports are not anonymous and tied to your reporter account for safety and accountablity.
+                        all reports are not anonymous and tied to your reporter account for safety and accountability.
                     </p>
                 </div>
                 {/* text - end */}
@@ -20,7 +20,7 @@ function IncidentForm() {
 
                     <div className="flex flex-col">
                         <label htmlFor="crimeType" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Crime Type</label>
-                        <input type="text" id="crimeType" placeholder="e.g. theft, assault, vandalism" className="lowercase w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        <input type="text" id="crimeType" name="crimeType" placeholder="e.g. theft, assault, vandalism" className="lowercase w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                     </div>
 
                     <div className="flex flex-col">
@@ -29,7 +29,7 @@ function IncidentForm() {
                     </div>
 
                     <div>
-                        <label htmlFor="county" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">County Crime was Commited</label>
+                        <label htmlFor="county" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">County Crime was Committed</label>
                         <input type="text" id="county" name="county" placeholder="e.g. dublin, cork, limerick, antrim" className="lowercase w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                     </div>
 
@@ -57,10 +57,10 @@ function IncidentForm() {
                         <button className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">
                             Send
                         </button>
-                        <span className="text-sm text-gray-500">*Required</span>
+                        <span className="text-sm text-gray-500">Required</span>
                     </div>
                     <p className="text-xs text-gray-400">
-                        By submiting this report you agree to our{" "}
+                        By submitting this report you agree to our{" "}
                         <a
                             href="#"
                             className="underline transition duration-100 hover:text-indigo-500 active:text-indigo-600"
