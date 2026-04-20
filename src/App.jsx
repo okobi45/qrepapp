1c1c44a98e19f49193b2f71b4683ec0d9fd43455
Okobi Eigbe <56739652+okobi45@users.noreply.github.com>
2026-04-20 03:03:12 +0100

    updated App.jsx, old route commented out

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter,
+  Routes,
+  Route
 } from "react-router-dom";
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import IncidentForm from './Pages/IncidentForm';
 import ReporterDash from './Pages/ReporterDash';
 import AdminDash from './Pages/AdminDash';
@@ -19,65 +20,78 @@ import Privacy from './Pages/Privacy';
 import Faqs from './Pages/Faqs';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "*",
-    element: <Error />,
-  },
-  {
-    path: "/incident",
-    element: <IncidentForm />,
-  },
-  {
-    path: "/rdashboard",
-    element: <ReporterDash />,
-  },
-  {
-    path: "/adminpanel",
-    element: <AdminDash />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/tos",
-    element: <Tos />,
-  },
-  {
-    path: "/privacy",
-    element: <Privacy />,
-  },
-  {
-    path: "/faqs",
-    element: <Faqs />,
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/about",
+//     element: <About />,
+//   },
+//   {
+//     path: "/contact",
+//     element: <Contact />,
+//   },
+//   {
+//     path: "*",
+//     element: <Error />,
+//   },
+//   {
+//     path: "/incident",
+//     element: <IncidentForm />,
+//   },
+//   {
+//     path: "/rdashboard",
+//     element: <ReporterDash />,
+//   },
+//   {
+//     path: "/adminpanel",
+//     element: <AdminDash />,
+//   },
+//   {
+//     path: "/login",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/register",
+//     element: <Register />,
+//   },
+//   {
+//     path: "/tos",
+//     element: <Tos />,
+//   },
+//   {
+//     path: "/privacy",
+//     element: <Privacy />,
+//   },
+//   {
+//     path: "/faqs",
+//     element: <Faqs />,
+//   }
+// ])
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/incident" element={<IncidentForm />} />
+        <Route path="/rdashboard" element={<ReporterDash />} />
+        <Route path="/adminpanel" element={<AdminDash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/tos" element={<Tos />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/faqs" element={<Faqs />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
