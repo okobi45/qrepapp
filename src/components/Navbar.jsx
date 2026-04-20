3cac64916424993f5c6ffe39aeccecd6190505a7
Okobi Eigbe <56739652+okobi45@users.noreply.github.com>
2026-04-20 03:31:25 +0100

    fixed Navbar structure, repaced a with Link, added Login and Register to Link and other style fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
+function Navbar() {
     return (
-        <div className="text-white bg-blue-500">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a href="/" className="mr-5 hover:text-gray-900">Home</a>
-                    <a href="/about" className="mr-5 hover:text-gray-900">About Us</a>
-                    <a href="/contact" className="mr-5 hover:text-gray-900">Contact Us</a>
-                </nav>
+        <nav className="bg-blue-500 py-6">
+            <div className="mx-auto max-w-screen-xl flex justify-between items-center">
+                <Link to="/" className="text-white text-3xl font-semibold px-6">
+                    SWR
+                </Link>
+                <div className="flex space-x-6">
+                    <Link to="/" className="text-white hover:text-gray-900">Home</Link>
+                    <Link to="/about" className="text-white hover:text-gray-900">About Us</Link>
+                    <Link to="/contact" className="text-white hover:text-gray-900">Contact Us</Link>
+                    <Link to="/login" className="text-white hover:text-gray-900">Login</Link>
+                    <Link to="/register" className="text-white hover:text-gray-900">Register</Link>
+                </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
+export default Navbar
